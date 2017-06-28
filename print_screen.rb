940407c379dce7156cd4aaf3d0ecf107135273c1
stephinwv <thekingdomseeker@aol.com>
2017-06-28 14:17:22 -0400

    exercise 4 complete

---
+++ print_screen.rb
@@ -0,0 +1,15 @@
+x = 0
+100.times do
+		x += 1
+		if (x % 3 == 0) && (x % 5 == 0)
+			puts "Mined Minds"
+		elsif (x % 3 == 0)
+			puts "mined" 
+		elsif 
+			(x % 5 == 0)
+			puts "minds"
+		else
+			p x
+		end
+end
+			
