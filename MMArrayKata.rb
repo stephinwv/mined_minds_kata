33d1a1dded01bc398a333d41094d34ecc69eaeb4
stephinwv <thekingdomseeker@aol.com>
2017-07-10 13:14:27 -0400

    Add files via upload

--- MMArrayKata.rb
+++ MMArrayKata.rb
@@ -4,12 +4,15 @@ def numbers()
 end
 def sequence
 	numbers.collect do |n|
-		if (n % 3 == 0)
+		if (n % 3 == 0 && n % 5 == 0)
+			"mined minds"
+		
+			elsif (n % 3 == 0)
 			"mined"
 		
-		else (n % 5 == 0)
-			"minds"
-		
+				else (n % 5 == 0)
+				"minds"
+				
 		end
 	end
 
--- MMArrayKatatest.rb
+++ MMArrayKatatest.rb
@@ -3,20 +3,24 @@ require_relative "MMArrayKata.rb"
 
 class TestMinedMindsKataArrayTest < Minitest::Test
   	def test_1_equals_1
-    assert_equal(10-5, 3+2)
+    	assert_equal(10-5, 3+2)
   	end
   	def test_count_100
-    results = numbers
-    assert_equal(100, results.count)
-    assert_equal(Array, results.class)
+    	results = numbers
+    	assert_equal(100, results.count)
+    	assert_equal(Array, results.class)
   	end
 	def test_third_number
-	assert_equal("mined", sequence[2])
+		assert_equal("mined", sequence[2])
 	end
 	def test_72_number
-	assert_equal("mined", sequence[71])
+		assert_equal("mined", sequence[71])
 	end
 	def test_fifth_number
-	assert_equal("minds", sequence[4])
+		assert_equal("minds", sequence[4])
 	end
+	def test_fifteen_number
+		assert_equal("mined minds", sequence[14])
+	end	
+
 end
